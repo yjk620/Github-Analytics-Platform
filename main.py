77196c59ad436974c7a796d8d9d2b2c497aeed1d
Kevin Kim <kevinkim0620@gmail.com>
2026-08-06 16:07:05 +0900

    feat: exchange OAuth code for Github access token

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from config import settings
+import httpx
+
 
 app = FastAPI()
 #hcheck health: checks if the app is running and returns the test_value from the .env file
@@ -13,3 +15,17 @@ def health_check():
 def auth_github():
   url = f"https://github.com/login/oauth/authorize?client_id={settings.github_client_id}&redirect_uri={settings.github_redirect_uri}&scope=public_repo"
   return RedirectResponse(url)
+
+#route #2: receives the code from GitHub, exchanges it for an access token
+@app.get("/auth/callback")
+def callback(code: str):
+  response = httpx.post(
+    "https://github.com/login/oauth/access_token",
+    data={
+      "client_id": settings.github_client_id,
+      "client_secret": settings.github_client_secret,
+      "code": code
+    },
+    headers={"Accept": "application/json"}
+  )
+  return {"access_token": response.json().get("access_token")}
